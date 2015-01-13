148d84a42bbd1df247667ce0a062f32cbc34ee1a
SachaCR <sachacr@gmail.com>
2015-01-14 00:00:43 +0100

    Add test to room and door models

--- tests/dungeonGen.js
+++ tests/dungeonGen.js
@@ -9,13 +9,12 @@ describe('dungeonGen', function(){
     it('should return a dungeon Map with 5 rooms', function(){
 
     	var dungeonMap = dungeonGen.generate(5);
-    	console.log(dungeonMap);
       expect(dungeonMap.length).to.be.equal(5);
       expect(dungeonMap[0].id).to.be.equal(0);
       expect(dungeonMap[1].id).to.be.equal(1);
       expect(dungeonMap[2].id).to.be.equal(2);
       expect(dungeonMap[3].id).to.be.equal(3);
       expect(dungeonMap[4].id).to.be.equal(4);
-    })
-  })
-})
+    });
+  });
+});
