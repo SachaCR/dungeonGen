2d47bc000525303c272f7ab57393c27183719c2f
Sacha CLERC-RENAUD <sclercrenaud.solutec@tf1.fr>
2015-01-14 12:14:12 +0100

    Add tests and config files

--- tests/dungeonGen.js
+++ tests/dungeonGen.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var expect = require('chai').expect;
+var rewire = require('rewire');
 
 var dungeonGen = require('../lib/dungeonGen');
 
 describe('dungeonGen', function() {
   describe(':generate(5)', function() {
-    it('should return a dungeon Map with 5 rooms', function() {
 
+    it('should return a dungeon Map with 5 rooms', function() {
     	var dungeonMap = dungeonGen.generate(5);
       expect(dungeonMap.length).to.be.equal(5);
       expect(dungeonMap[0].id).to.be.equal(0);
@@ -16,5 +17,29 @@ describe('dungeonGen', function() {
       expect(dungeonMap[3].id).to.be.equal(3);
       expect(dungeonMap[4].id).to.be.equal(4);
     });
+
+    it('Check if doors and rooms are consistent', function() {
+      var mockDungeonGen = rewire('../lib/dungeonGen');
+      var dungeonPath = ['north', 'east', 'south', 'west'];
+      var i = 0;
+      mockDungeonGen.__set__('internals.randDirection', function(){
+        var result = dungeonPath[i];
+        i++;
+        return result; 
+      });
+
+      var dungeonMap = mockDungeonGen.generate(5);
+      expect(dungeonMap.length).to.equal(5);
+      expect(dungeonMap[0].north).to.exist;
+      expect(dungeonMap[0].north.targetRoomId).to.equal(1);
+      expect(dungeonMap[1].south).to.exist;
+      expect(dungeonMap[1].south.targetRoomId).to.equal(0);
+
+      expect(dungeonMap[1].east).to.exist;
+      expect(dungeonMap[1].east.targetRoomId).to.equal(2);
+      expect(dungeonMap[2].west).to.exist;
+      expect(dungeonMap[2].west.targetRoomId).to.equal(1);
+    });
+
   });
 });
--- tests/models/door.js
+++ tests/models/door.js
@@ -7,9 +7,15 @@ var Door = require('../../lib/models/door');
 describe('dungeonGen', function() {
   describe(':constructor()', function() {
     it('should return a door with default params', function() {
-    	var door = new Door({ targetRoomId: 2 });
-      expect(door.targetRoomId).to.be.equal(2);
+    	var door = new Door();
+      expect(door.targetRoomId).to.be.equal(0);
       expect(door.locked).to.be.equal(false);
+    });
+
+    it('should return a door with override default params', function() {
+    	var door = new Door({ targetRoomId: 2, locked : true});
+      expect(door.targetRoomId).to.be.equal(2);
+      expect(door.locked).to.be.equal(true);
     });
   });
 });
