cf681d23277f786996fa741516363739e1b45d62
Sachacr <sachacr@runningheroes.com>
2015-12-11 09:30:38 +0100

    Add test error case on mover behavior and configure tape with istanbul

--- test/lib/behaviors/mover.test.js
+++ test/lib/behaviors/mover.test.js
@@ -21,3 +21,24 @@ test('Behavior : move', function (t) {
   t.equal(dungeon.currentRoom.id, 1, 'The current room has changed');
   t.end();
 });
+
+test('Behavior : move should return an error because there is no door to south', function (t) {
+  let dungeon = dungeonFactory.create(1, 5, commons);
+  let action = actionFactory.create('player', 'move', 'south', 'player', 5);
+  let moverObject = mover();
+  let result = moverObject.move(action, dungeon);
+  t.plan(1);
+  t.equal(result, 'There is no door in this direction', 'Ok this is the good message ');
+  t.end();
+});
+
+test('Behavior : move should return an error because the door is locked', function (t) {
+  let dungeon = dungeonFactory.create(1, 5, commons);
+  dungeon.currentRoom.north.locked = true;
+  let action = actionFactory.create('player', 'move', 'north', 'player', 5);
+  let moverObject = mover();
+  let result = moverObject.move(action, dungeon);
+  t.plan(1);
+  t.equal(result, 'The door is locked', 'Ok this is the good message ');
+  t.end();
+});
