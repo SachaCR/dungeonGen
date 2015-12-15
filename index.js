23c2753dbe8788412a0988190e299585ff017131
Sachacr <sachacr@runningheroes.com>
2015-12-15 11:23:42 +0100

    Update index.js call to dungeonFactory.create() by passing commons in params

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 let dungeonFactory = require('./lib/factories/dungeonFactory');
 let playerFactory = require('./lib/factories/playerFactory');
 let processGameState = require('./lib/core/processGameState');
+let commons = require('../lib/helpers/commons');
 
 function initGame(name, race, classe) {
-  let dungeon = dungeonFactory.create(3, 10, 15);
+  let dungeon = dungeonFactory.create(3, 10, commons);
   let player = playerFactory.create(name, race, classe);
   return processGameState(player, dungeon);
 }
